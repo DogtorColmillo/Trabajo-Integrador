0facc685711e4bf7aea99edd351d13478fdf2ad9
DogtorColmillo <martinvarela206@gmail.com>
2023-04-03 17:53:49 -0300

    Ya escribe en el word

--- src/main/java/com/trabajo_integrador/Word.java
+++ src/main/java/com/trabajo_integrador/Word.java
@@ -3,12 +3,20 @@ package com.trabajo_integrador;
 import java.io.FileOutputStream;
 
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
 
 public class Word {
    private XWPFDocument documento = new XWPFDocument();
    public void crear(String texto){
       try {
          FileOutputStream output = new FileOutputStream("resultados de pronosticos.docx");
+
+         XWPFParagraph parrafo = documento.createParagraph();
+         XWPFRun run = parrafo.createRun();
+
+         run.setText(texto);
+
          documento.write(output);
          output.close();
       } catch (Exception e) {
